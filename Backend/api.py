91140ea6381b50420f3eeed599c59675ccc4733b
Ameya Sindgikar <ameya-sindgikar@users.noreply.github.com>
2018-11-17 14:28:40 -0500

    Conect to sqlite instance and store some dummy data

--- Backend/api.py
+++ Backend/api.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-import pymongo
+import sqlite3
+import os.path
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, r"C:\sqlite3\dbs\GiftTheCode.db")
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,8 +15,14 @@ class Users (Resource):
         user = request.form
         user_json = user.to_dict()
 
-        print(user_json)
-       
+        print(user_json["Name"])
+        
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO Users VALUES(1, 'Basketball', 'Toronto', 3600)")
+        conn.commit()
+        conn.close()
+
         return jsonify({"Result":"Success"})
         
 api.add_resource(Users,'/Users/')
