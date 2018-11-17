86b52c93f388b2346831bd91e8e78ea388b67a43
James Vidler <james.vidler@outlook.com>
2018-11-17 17:45:02 -0500

    fixed share buttons

--- Frontend/src/Components/Share/share.js
+++ Frontend/src/Components/Share/share.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import './share.css';
-import {InlineShareButtons} from 'sharethis-reactjs';
-import App from '../App/App';
+import {
+    FacebookShareCount,
+    FacebookIcon,
+    TwitterShareButton,
+    TwitterIcon
+  } from 'react-share';
+import FacebookShareButton from 'react-share/lib/FacebookShareButton';
 
 class Share extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        friend: null
+        friend: 'empty',
+        friendSet: false
     }
 
     this.setFriend = this.setFriend.bind(this);
   }
 
   setFriend = (event) => {
-      this.setState({friend: event.target.value});
-      this.forceUpdate()
+      this.setState({friend: event.target.value, friendSet: true});
   }
 
   render() {
+
+    let shareButtons = null;
+
+    
+
     return (
       <div>
         <h3>Share</h3>
@@ -27,38 +37,28 @@ class Share extends React.Component {
             <input type='text' onKeyDown={this.setFriend} name='friend' />
         <p>Hello thats me{this.state.friend}</p>
 
-        <InlineShareButtons
-          config={{
-            alignment: 'center',  // alignment of buttons (left, center, right)
-            enabled: true,        // show/hide buttons (true, false)
-            font_size: 16,        // font size for the buttons
-            labels: 'cta',        // button labels (cta, counts, null)
-            language: 'en',       // which language to use (see LANGUAGES)
-            networks: [           // which networks to include (see SHARING NETWORKS)
-              'whatsapp',
-              'linkedin',
-              'messenger',
-              'facebook',
-              'twitter'
-            ],
-            padding: 12,          // padding within buttons (INTEGER)
-            radius: 4,            // the corner radius on each button (INTEGER)
-            show_total: true,
-            size: 40,             // the size of each button (INTEGER)
+        <TwitterShareButton
+            url={window.location.href}
+            title={this.state.friend}
+            className="social-icon">
+            <TwitterIcon
+              size={64}
+              round />
+          </TwitterShareButton>
 
-            // OPTIONAL PARAMETERS
-            url: window.location.href, // (defaults to current url)
-            image: '',  // (defaults to og:image or twitter:image)
-            description: this.props.setActivity,
-            title: 'I unplugged with '+ this.state.friend,
-            message: 'this is our email message text',     // (only for email sharing)
-            subject: 'this is our email subject text',  // (only for email sharing)
-            username: this.state.friend // (only for twitter sharing)
-          }}
-        />
+          <FacebookShareButton
+            url={window.location.href}
+            quote={this.state.friend}
+            className="social-icon">
+            <FacebookIcon
+              size={64}
+              round />
+          </FacebookShareButton>
+
+        
 
       </div>
-
+    
     );
   }
 }
