92df5d7850ccb7030ec4427f8eb86c6806ead445
mgknair <mgknair@uwaterloo.ca>
2018-11-18 03:12:28 -0500

    lat long being fetched and visualized

--- Visualization/shp.py
+++ Visualization/shp.py
@@ -11,13 +11,23 @@ df = pd.DataFrame({'City': ['Toronto', 'Calgary', 'Vancouver', 'Ottawa', 'St.Joh
 	'Latitude': [43.6532, 51.0486, 49.2827, 45.4315, 45.2733, 45.2733],
 	'Longitude': [-79.3832, -114.0708, -123.1207, -75.6972, -66.063, -66.063]})
 
+#df.loc[-1] = ["city",-33.00,-33.00]
+#df.index = df.index + 1
+
 connection = sqlite3.connect(r"C:\Users\Mahesh\Documents\GitHub\GiftTheCode2018\Backend\GiftTheCode.db")
-print (connection)
+#print (connection)
 cursor = connection.execute("select location,lat,long from Users")
-print (cursor)
+#print (cursor)
 
 row= cursor.fetchall()
-
+for i in row:
+	print (i)
+	if i[1]==None or i[2] == None:
+		pass
+	else:
+		df.loc[-1] = [i[0],i[1],i[2]]
+		df.index = df.index + 1
+print (df)
 
 df['Coordinates'] = list(zip(df.Longitude, df.Latitude))
 df['Coordinates'] = df['Coordinates'].apply(Point)
