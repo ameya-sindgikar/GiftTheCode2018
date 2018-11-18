ac299c91fb97bf645e4b9a7837441db884c53f77
mgknair <mgknair@uwaterloo.ca>
2018-11-18 11:02:44 -0500

    commented, and map visualized

--- Visualization/shp.py
+++ Visualization/shp.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import shapely
 from shapely.geometry import Point
+from pyproj import Proj, transform
 from shapely import wkt
 import sqlite3
 
@@ -13,7 +14,7 @@ df = pd.DataFrame({'City': ['Toronto', 'Calgary', 'Vancouver', 'Ottawa', 'St.Joh
 
 #df.loc[-1] = ["city",-33.00,-33.00]
 #df.index = df.index + 1
-
+#connect to our DB and get relavant values
 connection = sqlite3.connect(r"C:\Users\Mahesh\Documents\GitHub\GiftTheCode2018\Backend\GiftTheCode.db")
 #print (connection)
 cursor = connection.execute("select location,lat,long from Users")
@@ -25,8 +26,10 @@ for i in row:
 	if i[1]==None or i[2] == None:
 		pass
 	else:
-		df.loc[-1] = [i[0],i[1],i[2]]
-		df.index = df.index + 1
+		#check to see if point is in northwest quardrant only then add it to df
+		if 0.0 < i[1] <= 90.0 and -180.0 <= i[2] < 0:
+			df.loc[-1] = [i[0],i[1],i[2]]
+			df.index = df.index + 1
 print (df)
 
 df['Coordinates'] = list(zip(df.Longitude, df.Latitude))
@@ -36,14 +39,21 @@ print (df["Coordinates"])
 #gdf = geopandas.GeoDataFrame(df, geometry='Coordinates')
 shp = geopandas.read_file(r"C:\Users\Mahesh\Documents\GitHub\GiftTheCode2018\Visualization\Resources\gpr_000b11a_e.shp")
 #print (shp.columns)
-print (shp["PRENAME"])
+# print (Proj(shp.crs))
+# print (shp["PRENAME"])
 
-
-for point in df["Coordinates"]:
-	for province in shp.geometry:
-		print (point.within(province))
+# for point in df["Coordinates"]:
+# 	for province in shp.geometry:
+# 		print (point.within(province))
 
 
 shp.plot(linestyle="-.", linewidth=0.1);
-plt.plot(df.Longitude,df.Latitude,'g*')
-plt.show();
+plt.plot(df.Longitude,df.Latitude,'r*')
+plt.title("Unplugged and connect participants")
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.savefig("map.png")
+plt.show()
+
+
+
